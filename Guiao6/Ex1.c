6ea27d88d4c71dc9176c60027aa0f633a2fedd8f
Whitesnake <robertograca@ua.pt>
2020-04-20 14:44:11 +0100

    Código da parte2 Guiao6

--- Guiao6/Ex1.c
+++ Guiao6/Ex1.c
@@ -14,12 +14,29 @@ int motzkinRec(int n){
     }
 }
 
+int motzkinPD(int* din, int n){
+    if(n<2)return 1;
+    else{
+        int i,sum=0;
+        for(i=0;i<=n-2;i++){
+            nops++;
+            sum += din[i]*din[n-2-i];
+        }
+        return motzkinPD(din,n-1)+sum;
+    }
+}
+
 int main(void){
     int i=0;
-    long res;
+    int resRec;
+    int din[15];
+    printf("\tResRec   NopsRec   ResPD    NopsPD\n");
     for(;i<16;i++){
-        res = motzkinRec(i);
-        printf("%2d -> Resultado = %8ld | NOps = %8ld\n",i,res,nops);
+        resRec = motzkinRec(i);
+        printf("%2d -> %8d %8ld",i,resRec,nops);
+        nops = 0;
+        din[i] = motzkinPD(din,i);
+        printf("%8d %8ld\n",din[i],nops);
         nops = 0;
     }
     return 0;
