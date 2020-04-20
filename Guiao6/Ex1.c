209c3b89adc2c0831335a6130a45487510470dae
Whitesnake <robertograca@ua.pt>
2020-04-20 15:22:51 +0100

    Parte2 Guiao6

--- Guiao6/Ex1.c
+++ Guiao6/Ex1.c
@@ -14,29 +14,35 @@ int motzkinRec(int n){
     }
 }
 
-int motzkinPD(int* din, int n){
-    if(n<2)return 1;
-    else{
-        int i,sum=0;
-        for(i=0;i<=n-2;i++){
+int motzkinPD(int n){
+    int din[n+1];
+
+    din[0] = din[1] = 1;
+
+    int k,i,sum=0;
+
+    for(i=2;i<=n;i++){
+        sum = 0;
+        for(k=0;k<=i-2;k++){
             nops++;
-            sum += din[i]*din[n-2-i];
+            sum += din[k]*din[i-2-k];
         }
-        return motzkinPD(din,n-1)+sum;
+        din[i] = din[i-1] + sum;
     }
+
+    return din[n];    
 }
 
 int main(void){
     int i=0;
-    int resRec;
-    int din[15];
+    int resRec,resPD;
     printf("\tResRec   NopsRec   ResPD    NopsPD\n");
     for(;i<16;i++){
         resRec = motzkinRec(i);
         printf("%2d -> %8d %8ld",i,resRec,nops);
         nops = 0;
-        din[i] = motzkinPD(din,i);
-        printf("%8d %8ld\n",din[i],nops);
+        resPD = motzkinPD(i);
+        printf("%8d %8ld\n",resPD,nops);
         nops = 0;
     }
     return 0;
